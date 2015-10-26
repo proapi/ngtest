f149d495bbdf94cf216ce88d33ac41182cd16827
Michał Pawelski <kontakt@proapi.eu>
2015-10-26 14:00:54 +0100

    Before adding bower

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,6 +4,11 @@ angular.module('ngtest', ['ui.router'])
       '$urlRouterProvider',
       function ($stateProvider, $urlRouterProvider) {
         $stateProvider
+            .state('posts', {
+              url: '/posts/{id}',
+              templateUrl: '/posts.html',
+              controller: 'PostsCtrl'
+            })
             .state('home', {
               url: '/home',
               templateUrl: '/home.html',
@@ -34,7 +39,11 @@ angular.module('ngtest', ['ui.router'])
           $scope.posts.push({
             title: $scope.title,
             link: $scope.link,
-            upvotes: 0
+            upvotes: 0,
+            comments: [
+              {author: 'Joe', body: 'Cool post!', upvotes: 0},
+              {author: 'Bob', body: 'Gread idea, but everything is wrong!', upvotes: 0}
+            ]
           });
           $scope.title = '';
           $scope.link = '';
@@ -42,5 +51,23 @@ angular.module('ngtest', ['ui.router'])
 
         $scope.incrementUpvotes = function (post) {
           post.upvotes += 1;
+        };
+      }])
+    .controller('PostsCtrl', [
+      '$scope',
+      '$stateParams',
+      'posts',
+      function ($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+        $scope.addComment = function () {
+          if ($scope.body === '') {
+            return;
+          }
+          $scope.post.comments.push({
+            body: $scope.body,
+            author: 'user',
+            upvotes: 0
+          });
+          $scope.body = '';
         };
       }]);
